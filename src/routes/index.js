5a659cd555acf566ce1851ead7b0697f86e0c3fb
Arslan Tiwana <muhammadarslan0111@gmail.com>
2024-01-21 02:32:59 +0500

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 
 router.use("/user", require("./user"));
 router.use("/client", require("./client"));
-router.use("/color_print", require("./colorPrint"));
+router.use("/colorprint", require("./colorPrint"));
 router.use("/film", require("./film"));
 router.use("/offset", require("./offset"));
 router.use("/order", require("./order"));
 router.use("/panaflex", require("./panaflex"));
 router.use("/plates", require("./plates"));
-router.use("/wedding_card", require("./weddingCard"));
+router.use("/weddingcard", require("./weddingCard"));
 
 module.exports = router;
